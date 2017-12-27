952798241d300fae632d8f2a6c2996befba487ff
Sanja Drobac <33283592+SanjaAra@users.noreply.github.com>
2017-12-28 00:57:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,76 @@
-let mainContent = document.querySelector('#main-content'),
-  snow = document.querySelectorAll('.snow')[0],
-  times = document.querySelectorAll('.time'),
-  nyMilisec = Date.parse("2018-01-01T00:00:00"),
-  today = new Date().getTime(),
-  toNYtime = nyMilisec - today;
+var wrapp = document.querySelector('.wrapp');
+var winner = document.querySelector('.winner');
+var xo = "O";
+var endGame = false;
 
-countdown(toNYtime);
+createTable();
 
-setInterval(function() {
-  today = new Date().getTime();
-  toNYtime = nyMilisec - today;
-  countdown(toNYtime);
-}, 1000)
+var boxes = document.getElementsByClassName('box');
 
-function countdown(ms) {
-  let day = Math.floor(ms / (24 * 60 * 60 * 1000)),
-    dayMsRest = ms % (24 * 60 * 60 * 1000),
-    our = Math.floor(dayMsRest / (60 * 60 * 1000)),
-    ourMsRest = dayMsRest % (60 * 60 * 1000),
-    min = Math.floor(ourMsRest / (60 * 1000)),
-    minMsRest = ourMsRest % (60 * 1000),
-    sec = Math.floor(minMsRest / 1000);
+var lines = [
+    [boxes[0], boxes[1], boxes[2]],
+    [boxes[3], boxes[4], boxes[5]],
+    [boxes[6], boxes[7], boxes[8]],
+    [boxes[0], boxes[3], boxes[6]],
+    [boxes[1], boxes[4], boxes[7]],
+    [boxes[2], boxes[5], boxes[8]],
+    [boxes[0], boxes[4], boxes[8]],
+    [boxes[2], boxes[4], boxes[6]]
+]
 
-  (day < 10) ? day = '0' + day: day;
-  (our < 10) ? our = '0' + our: our;
-  (min < 10) ? min = '0' + min: min;
-  (sec < 10) ? sec = '0' + sec: sec;
-
-  times[0].innerHTML = day;
-  times[1].innerHTML = our;
-  times[2].innerHTML = min;
-  times[3].innerHTML = sec;
+for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', insertSymbol)
 }
 
-(function snowflake() {
-  let h = window.innerWidth;
-  snow.style.height = h + 'px';
-  console.log(h);
-}());
+function insertSymbol() {
+    (xo == "O") ? xo = "X": xo = "O";
+    this.innerHTML = xo;
+    this.classList.add('active');
+    if (!endGame) {
+        compMove();
+        checkLines();
+    }
+
+}
+
+function checkLines() {
+    for (let i = 0; i < lines.length; i++) {
+        let box1 = lines[i][0];
+        let box2 = lines[i][1];
+        let box3 = lines[i][2];
+        if (box1.innerHTML == box2.innerHTML && box1.innerHTML == box3.innerHTML && box1.innerHTML != "") {
+            box1.style.background = "tomato";
+            box2.style.background = "tomato";
+            box3.style.background = "tomato";
+            winner.innerHTML = "<h1>Winner is " + box1.innerHTML + "</h1>";
+            endGame = true;
+        }
+    }
+}
+
+
+
+function createTable() {
+    var text = '';
+    for (let i = 0; i < 9; i++) {
+        text += '<div class="box"></div>';
+    }
+    wrapp.innerHTML = text;
+}
+
+function compMove() {
+    (xo == "O") ? xo = "X": xo = "O";
+    var emptyFields = [];
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].innerHTML == "") {
+            emptyFields.push(boxes[i])
+        }
+    }
+    var rand = Math.floor(Math.random() * emptyFields.length);
+    setTimeout(() => {
+        emptyFields[rand].classList.add('active');
+        emptyFields[rand].innerHTML = xo;
+        checkLines()
+    }, 500)
+
+}
